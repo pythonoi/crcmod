4c6de580d2810e2cbf3b330a6d66a496174b0097
rlb <rlb>
2004-05-22 18:04:34 +0000

    Add Crc class

--- crcmod.py
+++ crcmod.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------------
-# crcmod is a Python module for gererating functions that compute the Cyclic
+# crcmod is a Python module for gererating objects that compute the Cyclic
 # Redundancy Check.  Any 8, 16, 32, or 64 bit polynomial can be used.  
 #
 # Copyright (c) 2004  Raymond L. Buvel
@@ -22,20 +22,130 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 #-----------------------------------------------------------------------------
-__all__ = '''
+__all__ = '''mkCrcFun
 '''.split()
 
 # Select the appropriate set of low-level CRC functions for this installation.
-# If the extension module was not built, drop back on the Python implementation
+# If the extension module was not built, drop back to the Python implementation
 # even though it is significantly slower.
 try:
     import _crcfunext as _crcfun
-    usingExtension = True
+    _usingExtension = True
 except ImportError:
     import _crcfunpy as _crcfun
-    usingExtension = False
+    _usingExtension = False
 
 import sys, struct
+
+#-----------------------------------------------------------------------------
+class Crc:
+    def __init__(self, poly, initCrc=~0L, rev=True, initialize=True):
+        if not initialize:
+            return
+
+        x = _mkCrcFun(poly, initCrc, rev)
+        self._crc = x[0]
+        self.digest_size = x[1]//8
+        self.initCrc = x[2]
+        self.table = x[3]
+        self.crcValue = self.initCrc
+        self.reverse = rev
+        self.poly = poly
+
+    def new(self, arg=None):
+        n = Crc(poly=None, initialize=False)
+        n._crc = self._crc
+        n.digest_size = self.digest_size
+        n.initCrc = self.initCrc
+        n.table = self.table
+        n.crcValue = self.initCrc
+        n.reverse = self.reverse
+        n.poly = self.poly
+        if arg is not None:
+            n.update(arg)
+        return n
+
+    def copy(self):
+        c = self.new()
+        c.crcValue = self.crcValue
+        return c
+
+    def update(self, data):
+        self.crcValue = self._crc(data, self.crcValue)
+
+    def digest(self):
+        n = self.digest_size
+        crc = self.crcValue
+        lst = []
+        while n > 0:
+            lst.append(chr(crc & 0xFF))
+            crc = crc >> 8
+            n -= 1
+        lst.reverse()
+        return ''.join(lst)
+
+    def hexdigest(self):
+        n = self.digest_size
+        crc = self.crcValue
+        lst = []
+        while n > 0:
+            lst.append('%02X' % (crc & 0xFF))
+            crc = crc >> 8
+            n -= 1
+        lst.reverse()
+        return ''.join(lst)
+
+    def generateCode(self, functionName, out, dataType=None, crcType=None):
+        if dataType is None:
+            dataType = 'UINT8'
+
+        if crcType is None:
+            crcType = 'UINT%d' % (8*self.digest_size)
+
+        if self.digest_size == 1:
+            crcAlgor = 'table[*data ^ (%s)crc]'
+        elif self.reverse:
+            crcAlgor = 'table[*data ^ (%s)crc] ^ (crc >> 8)'
+        else:
+            shift = 8*(self.digest_size - 1)
+            crcAlgor = 'table[*data ^ (%%s)(crc >> %d)] ^ (crc << 8)' % shift
+
+        fmt = '0x%%0%dXU,' % (2*self.digest_size)
+        n = {1:8, 2:8, 4:4, 8:2}[self.digest_size]
+
+        lst = []
+        for i, val in enumerate(self.table):
+            if (i % n) == 0:
+                lst.append('\n    ')
+            lst.append(fmt % val)
+
+        poly = 'polynomial: 0x%X' % self.poly
+        if self.reverse:
+            poly = poly + ', bit reverse algorithm'
+
+        parms = {
+            'dataType' : dataType,
+            'crcType' : crcType,
+            'name' : functionName,
+            'crcAlgor' : crcAlgor % dataType,
+            'crcTable' : ''.join(lst),
+            'poly' : poly,
+        }
+        out.write(_codeTemplate % parms) 
+
+#-----------------------------------------------------------------------------
+def mkCrcFun(poly, initCrc=~0L, rev=True):
+    '''Return a function that computes the CRC using the specified polynomial.
+
+    poly - integer representation of the generator polynomial
+    initCrc - default initial CRC value
+    rev - when true, indicates that the data is processed bit reversed.
+
+    The returned function has the following user interface
+    def crcfun(data, crc=initCrc):
+    '''
+
+    return _mkCrcFun(poly, initCrc, rev)[0]
 
 #-----------------------------------------------------------------------------
 # Naming convention:
@@ -54,7 +164,7 @@ def _verifyPoly(poly):
         high = low*2
         if low <= poly < high:
             return n
-    raise msg
+    raise ValueError(msg)
 
 #-----------------------------------------------------------------------------
 # Bit reverse the input value.
@@ -134,40 +244,67 @@ _sizeMap = {
 }
 
 #-----------------------------------------------------------------------------
+# Build a mapping of size to struct module type code.  This table is
+# constructed dynamically so that it has the best chance of picking the best
+# code to use for the platform we are running on.  This should properly adapt
+# to 32 and 64 bit machines.
+
+_sizeToTypeCode = {}
+
+for typeCode in 'B H I L Q'.split():
+    size = {1:8, 2:16, 4:32, 8:64}.get(struct.calcsize(typeCode),None)
+    if size is not None and size not in _sizeToTypeCode:
+        _sizeToTypeCode[size] = '256%s' % typeCode
+
+del typeCode, size
+
+#-----------------------------------------------------------------------------
 # The following function returns a Python function to compute the CRC.  The
-# returned function calls a routine written in C to efficiently do the
-# calculation.
+# returned function calls a low level function that is written in C if the
+# extension module could be loaded.  Otherwise, a Python implementation is
+# used.  In addition to this function, the size of the CRC, the initial CRC,
+# and a list containing the CRC table are returned.
 
-def mkCrcFun(poly, initCrc=0, rev=False):
-    '''Return a function that computes the CRC using the specified polynomial.
-    poly - integer representation of the generator polynomial
-    initCrc - default initial CRC value
-    rev - when true, indicates that the data is processed bit reversed.
+def _mkCrcFun(poly, initCrc, rev):
+    size = _verifyPoly(poly)
 
-    The returned function has the following user interface
-    def crcfun(data, crc=initCrc):
-    '''
-
-    n = _verifyPoly(poly)
+    # Adjust the initial CRC to the correct data type (unsigned value).
+    mask = (1L<<size) - 1
+    initCrc = long(initCrc) & mask
+    if mask <= sys.maxint:
+        initCrc = int(initCrc)
 
     if rev:
-        _table = _mkTable_r(poly, n)
-        _fun = _sizeMap[n][1]
+        tableList = _mkTable_r(poly, size)
+        _fun = _sizeMap[size][1]
     else:
-        _table = _mkTable(poly, n)
-        _fun = _sizeMap[n][0]
+        tableList = _mkTable(poly, size)
+        _fun = _sizeMap[size][0]
 
-    if usingExtension:
-        tableType = {
-             8 : '256B',
-            16 : '256H',
-            32 : '256L',
-            64 : '256Q',
-        }[n]
-        _table = struct.pack(tableType, *_table)
+    _table = tableList
+    if _usingExtension:
+        _table = struct.pack(_sizeToTypeCode[size], *tableList)
 
     def crcfun(data, crc=initCrc, table=_table, fun=_fun):
         return fun(data, crc, table)
 
-    return crcfun
+    return crcfun, size, initCrc, tableList
+
+#-----------------------------------------------------------------------------
+_codeTemplate = '''// Automatically generated CRC function
+// %(poly)s
+%(crcType)s
+%(name)s(%(dataType)s *data, int len, %(crcType)s crc)
+{
+    static const %(crcType)s table[256] = {%(crcTable)s
+    };
+  
+    while(len > 0) {
+        crc = %(crcAlgor)s;
+        data++;
+        len--;
+    }
+    return crc;
+}
+'''
 
