0b29dd7e92666dd5635eb48c9bda978c86d1ce61
rlb <rlb>
2006-04-22 21:48:43 +0000

    Enhance the package structure.

--- mkDist.py
+++ mkDist.py
@@ -1,37 +1,79 @@
-import tarfile, os
+import os, shutil
 
-version = '1.2'
+version = '1.3'
 
-arcname = 'crcmod-%s.tar.gz' % version
-arcdir = 'crcmod-%s' % version
+curdir = os.getcwd()
+crcdir = os.path.join(curdir,'dist/crcmod-%s' % version)
+moddir = os.path.join(crcdir,'crcmod')
+srcdir = os.path.join(crcdir,'src')
+testdir = os.path.join(crcdir,'test')
 
-files = '''
-README
-_crcfunpy.py
-crcmod.py
-test_crcmod.py
-timing_test.py
-'''.split()
+#-----------------------------------------------------------------------------
+# Clean out any previous distribution and create the base directories.
+if os.path.isdir(crcdir):
+    shutil.rmtree(crcdir)
 
-extfiles = '''
-Makefile
-_crcfunext.c
-setup.py
-'''.split()
+os.makedirs(crcdir)
+os.makedirs(moddir)
+os.makedirs(srcdir)
+os.makedirs(testdir)
 
-def addFiles(arc, dir, arcdir, names):
-    for name in names:
-        fname = os.path.join(dir,name)
-        arcname = os.path.join(arcdir,name)
-        arc.add(fname, arcname)
+#-----------------------------------------------------------------------------
+def copy(fname, dir):
+    dst = os.path.join(dir,fname)
+    src = os.path.join(curdir,fname)
+    shutil.copyfile(src, dst)
 
-arc = tarfile.open(arcname, 'w:gz')
-os.chdir('..')
+#-----------------------------------------------------------------------------
 
-arc.add('crcpkg', arcdir, recursive=False)
-addFiles(arc, 'crcpkg', arcdir, files)
+copy('README', crcdir)
+copy('changelog', crcdir)
 
-arc.add('crcpkg/extmod', arcdir+'/extmod', recursive=False)
-addFiles(arc, 'crcpkg/extmod', arcdir+'/extmod', extfiles)
-arc.close()
+copy('crcmod.py', moddir)
+copy('_crcfunpy.py', moddir)
 
+fd = open(os.path.join(moddir,'__init__.py'),'w')
+fd.write('''from crcmod import *
+__doc__ = crcmod.__doc__
+''')
+fd.close()
+
+shutil.copyfile('extmod/_crcfunext.c', os.path.join(srcdir,'_crcfunext.c'))
+
+copy('test_crcmod.py', testdir)
+copy('timing_test.py', testdir)
+
+basic = '''setup(
+name='crcmod',
+version='%s',
+description='CRC Generator',
+author='Ray Buvel',
+author_email='rlbuvel@gmail.com',
+url='http://crcmod.sourceforge.net/',
+packages=['crcmod'],
+''' % version
+
+setup = '''from distutils.core import setup
+from distutils.extension import Extension
+
+%s
+ext_modules=[ 
+    Extension('crcmod._crcfunext', ['src/_crcfunext.c', ],
+    ),
+],
+)
+''' % basic
+
+setup_py = '''from distutils.core import setup
+
+%s
+)
+''' % basic
+
+fd = open(os.path.join(crcdir,'setup.py'),'w')
+fd.write(setup)
+fd.close()
+
+fd = open(os.path.join(crcdir,'setup_py.py'),'w')
+fd.write(setup_py)
+fd.close()
