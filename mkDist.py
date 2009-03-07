be1d2228f3c7d5763858ed57952b3be76e72e385
rlb <rlb>
2009-03-07 19:13:28 +0000

    Make the package initialization work for both Python 2.x and Python 3.x

--- mkDist.py
+++ mkDist.py
@@ -56,7 +56,11 @@ copy3('_crcfunpy.py', moddir3)
 init_file = os.path.join(moddir,'__init__.py')
 
 fd = open(init_file,'w')
-fd.write('''from crcmod.crcmod import *
+fd.write('''try:
+    from crcmod.crcmod import *
+except ImportError:
+    # Make this backward compatible
+    from crcmod import *
 __doc__ = crcmod.__doc__
 ''')
 fd.close()
