8159cb43a38539c20a9e8c3d069fd39bc90c8f90
Ray Buvel <rlbuvel@gmail.com>
2010-06-18 08:19:45 -0500

    Updated unit tests from Craig McQueen.

--- test/test_crcmod.py
+++ test/test_crcmod.py
@@ -1,20 +1,6 @@
-import imp
-import os
-import sys
 
-if sys.version_info[0] == 2:
-    version_dir = 'python2'
-elif sys.version_info[0] == 3:
-    version_dir = 'python3'
+import unittest
 
-print(version_dir)
+import crcmod.test
 
-test_file_name = os.path.join(
-    os.path.dirname(__file__),
-    '..',
-    version_dir,
-    'test',
-    'test_crcmod.py'
-)
-
-imp.load_source("test_crcmod", test_file_name)
+unittest.main(module=crcmod.test)
